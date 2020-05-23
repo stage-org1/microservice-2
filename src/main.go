d319c5a9ce45b3457ad4f6e471a9037707e27458
beppe <beppe.vanrolleghem@gmail.com>
2020-05-23 13:51:09 +0200

    felix

--- src/main.go
+++ src/main.go
@@ -8,11 +8,12 @@ import (
 func main() {
 	http.HandleFunc("/", reply)
 	err:= http.ListenAndServe(":80", nil)
-	if err != nil {
+	if (err != nil) {
 		fmt.Println(err.Error())
 	}
 }
 
 func reply(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello world")
 	fmt.Fprintf(w, "hello world")
 }
